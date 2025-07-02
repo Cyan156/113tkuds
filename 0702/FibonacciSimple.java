cf42f8f849979b7764506a5ed17d916e1d8b4689
Cyan156 <daniellee1156@gmail.com>
2025-07-02 11:13:13 +0800

    0702練習

---
+++ 0702/FibonacciSimple.java
@@ -0,0 +1,9 @@
+public class FibonacciSimple {
+    public static void main(String[] args) {
+        int n = 10;
+        int a = 0, b = 1;
+
+        System.out.println("費氏數列前"+ n +"項:");
+        for(int)
+    }
+}
